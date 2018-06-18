4d299ae347cb5a9141e9588ddb9efc3e95398e7e
zhangxk <zhangxkmail@163.com>
2018-06-18 11:13:20 +0800

    add

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -11,6 +11,7 @@
   import HomeHeader from './components/Header'
   import HomeSwiper from './components/Swiper'
   import HomeIcons from './components/Icons'
+  import axios from 'axios'
 
   export default {
     name: 'Home',  // 定义组件名字
@@ -19,6 +20,18 @@
       HomeSwiper,
       HomeIcons
     },
+    mounted() {
+      this.getHomeInfo()
+    },
+    methods: {
+      getHomeInfo() {
+        axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+      },
+      getHomeInfoSucc(res) {
+        console.log(res)
+      }
+    }
   }
 </script>
 
